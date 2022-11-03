81c407b11e742f2f1cb3afa46f4f1e366aef3f8e
faga <lzc295592@163.com>
2022-11-03 21:06:32 +0800

    chore: template update

--- src/template/welcome.vue
+++ src/template/welcome.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
 import { ref, version as vueVersion } from 'vue'
-import { NInput } from 'naive-ui'
 const msg = ref('Hello World!')
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-  <n-input v-model="msg" />
-
-
+  <n-input v-model:value="msg" />
 </template>
